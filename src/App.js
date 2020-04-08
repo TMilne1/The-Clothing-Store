918ba5ed913594c41e65560c9f750b842c4f8727
tmilne1 <tmilne1@gmail.com>
2020-04-08 00:33:56 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import logo from './logo.svg';
 import Homepage from './pages/homepage/homepage.js';
 import Shop from './pages/shop/shop';
+import Header from './components/header/header'
 import './App.css'
 import {Switch, Route} from 'react-router-dom'
 
@@ -11,6 +12,7 @@ import {Switch, Route} from 'react-router-dom'
 function App() {
   return (
     <div >
+      <Header></Header>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={Shop} />
