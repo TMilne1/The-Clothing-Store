92932259677dcbdae5d65ffc8e4ccd48ff5c618f
tmilne1 <tmilne1@gmail.com>
2020-04-10 01:42:58 -0400

    integrated firebase in application,created signin page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import Homepage from './pages/homepage/homepage.js';
 import Shop from './pages/shop/shop';
 import Header from './components/header/header'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 import './App.css'
 import {Switch, Route} from 'react-router-dom'
+import {auth} from './firebase/firbase.utils'
 
 
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      currentUser:null
+    }
+  }
 
+  unsubscribeFromAuth = null;
 
-function App() {
-  return (
-    <div >
-      <Header></Header>
-      <Switch>
-        <Route exact path='/' component={Homepage}/>
-        <Route exact path='/shop' component={Shop} />
-      </Switch>
-    </div>
-  );
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=>{
+      console.log(user)
+      this.setState({currentUser:user})
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div >
+        <Header currentUser={this.state.currentUser}></Header>
+        <Switch>
+          <Route exact path='/' component={Homepage}/>
+          <Route exact path='/shop' component={Shop} />
+          {//<Route exact path='/contact' component={Contact} />
+          }
+          <Route exact path='/signIn' component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
+      
 }
 
 export default App;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './header.styles.scss'
 import {Link} from 'react-router-dom'
 import {ReactComponent as Logo} from '../../../src/assets/crown.svg'
+import {auth} from '../../firebase/firbase.utils'
 
-const Header =()=>{
+const Header =({currentUser})=>{
         return(
             <div className='header'>
                 <Link className='logo-container' to='/'>
@@ -12,7 +13,14 @@ const Header =()=>{
                 <div className='options'>
                     <Link className='option' to='/shop'>SHOP</Link>
                     <Link className='option' to='/contact'>CONTACT</Link>
-
+                    
+                
+                {
+                    currentUser ?
+                        <div className='option' onClick={()=>auth.signOut()}>SIGN OUT </div>
+                        :
+                        <Link className='option' is to='/signin'>SIGN IN</Link>
+                }
                 </div>
             </div>
         )
