2e07d9221942d50e3f6affb98777336960ba0ff7
tmilne1 <tmilne1@gmail.com>
2020-05-05 22:32:44 -0400

    updated app with rselect , replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -3,12 +3,15 @@ import './cart-icon.styles.scss'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
 
-const CartIcon = ({toggleCartHidden}) =>{
+const CartIcon = ({toggleCartHidden, quantity}) =>{
 return(
     <div className='cart-icon' onClick={toggleCartHidden}> 
         <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>1</span>
+            <span className='item-count'>
+                {quantity}
+            </span>
         
     </div>
 
@@ -16,6 +19,9 @@ return(
 
 }
 
+const mapStateToProps = (state) => ({
+    quantity: selectCartItemsCount(state) 
+})
 
 const mapDispatchToProps= dispatch => {
     return{
@@ -25,4 +31,4 @@ const mapDispatchToProps= dispatch => {
 
 
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
